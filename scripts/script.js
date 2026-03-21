a3572f6dd526608deae7855a42471333741b4777
grace-cecelia <gjulianna2000@gmail.com>
2026-03-21 15:00:25 -0400

    path test

---
+++ scripts/script.js
@@ -0,0 +1,7 @@
+var animation = bodymovin.loadAnimation({
+    container: document.getElementById("path-test-animation"),
+    renderer: "svg",
+    loop: true,
+    autoplay: true,
+    path: "garden_path_bodymovin_003.json"
+})
